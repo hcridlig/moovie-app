cf676d859e7227b57e621cc43e96eae8cfd307c8
Hugo CRIDLIG <80850294+hcridlig@users.noreply.github.com>
2024-10-23 11:59:43 +0000

    controller mongo -> postegres

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,12 +1,43 @@
-// backend/models/User.js
-const mongoose = require('mongoose');
+const { DataTypes, Sequelize } = require('sequelize');
+const config = require('../config/db');  // Reference to your Sequelize database connection
 
-const UserSchema = new mongoose.Schema({
-  username: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  watchedItems: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }],
-  platforms: [String],
+const sequelize = new Sequelize(config.development);
+
+const User = sequelize.define('user', {
+  iduser: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+    validate: {
+      isEmail: true,
+    },
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  role: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    defaultValue: 'user',  // e.g. 'admin', 'user', etc.
+  },
+}, {
+  tableName: 'users',  // Optional: defines the table name
+  timestamps: true,    // Adds `createdAt` and `updatedAt` fields
 });
 
-module.exports = mongoose.model('User', UserSchema);
+module.exports = {
+  User, 
+  sequelize
+ };
