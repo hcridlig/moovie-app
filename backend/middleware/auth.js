14688e6a67970c43074570c9456a68f9946e8fd1
Hugo CRIDLIG <80850294+hcridlig@users.noreply.github.com>
2024-10-25 14:00:56 +0000

    repair get and modif account

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -2,13 +2,18 @@
 const jwt = require('jsonwebtoken');
 
 function authMiddleware(req, res, next) {
-  const token = req.header('Authorization');
-  if (!token) {
+  const authHeader = req.header('Authorization');
+  if (!authHeader) {
     return res.status(401).json({ message: 'Accès refusé. Aucun token fourni.' });
+  }
+  
+  const token = authHeader.split(' ')[1]; // Get the token after "Bearer"
+  if (!token) {
+    return res.status(401).json({ message: 'Accès refusé. Token manquant.' });
   }
 
   try {
-    const decoded = jwt.verify(token, 'votre_secret_jwt');
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded;
     next();
   } catch (error) {
