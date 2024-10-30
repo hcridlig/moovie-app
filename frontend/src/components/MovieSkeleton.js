a61007aa04379d1da60944f8bded20fa4a94a197
AlexD4V <alex4921@hotmail.fr>
2024-10-30 17:30:10 +0100

    commit skeleton homepage

--- frontend/src/components/MovieSkeleton.js
+++ frontend/src/components/MovieSkeleton.js
@@ -4,10 +4,10 @@ import React from 'react';
 
 function MovieSkeleton() {
   return (
-    <div className="w-48 h-64 bg-gray-300 dark:bg-gray-700 animate-pulse rounded-lg">
-      <div className="h-40 bg-gray-400 dark:bg-gray-600 rounded-t-lg"></div>
-      <div className="p-3 space-y-2">
-        <div className="h-4 bg-gray-400 dark:bg-gray-600 rounded"></div>
+    <div className="w-48 mx-4 bg-white dark:bg-gray-800 shadow-lg rounded-lg overflow-hidden transform transition duration-300 animate-pulse">
+      <div className="h-64 bg-gray-300 dark:bg-gray-700"></div>
+      <div className="p-2">
+        <div className="h-4 bg-gray-400 dark:bg-gray-600 rounded mb-1"></div>
         <div className="h-4 bg-gray-400 dark:bg-gray-600 rounded w-3/4"></div>
       </div>
     </div>
