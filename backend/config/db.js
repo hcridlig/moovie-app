af6ae6df761ab00e5558281a5800165142e7a923
xanax971 <127352439+xanax971@users.noreply.github.com>
2024-11-11 19:32:48 +0100

    ajout de db.env /

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,21 +1,31 @@
 const dotenv = require('dotenv');
+const { Sequelize } = require('sequelize');
 
 // Load environment variables from .env file
 dotenv.config();
 
-module.exports = {
-  development: {
-    username: process.env.PGUSERNAME,
-    password: process.env.PGPASSWORD,
-    database: process.env.PGDATABASE,
+// Créer une instance Sequelize avec les informations de configuration
+const sequelize = new Sequelize(
+  process.env.PGDATABASE,
+  process.env.PGUSERNAME,
+  process.env.PGPASSWORD,
+  {
     host: process.env.PGHOST,
     dialect: 'postgres',
     port: process.env.DB_PORT || 5432,
     dialectOptions: {
       ssl: {
         require: true,
-        rejectUnauthorized: false, // This line is crucial for local development
+        rejectUnauthorized: false, // Utile pour le développement local
       },
     },
-  },
-};
+  }
+);
+
+// Tester la connexion
+sequelize
+  .authenticate()
+  .then(() => console.log('Connexion réussie à PostgreSQL'))
+  .catch(err => console.error('Impossible de se connecter à PostgreSQL', err));
+
+module.exports = sequelize;
