0badeea4ce5f4e5922e2cc8332053661b9b3b49b
Hugo CRIDLIG <80850294+hcridlig@users.noreply.github.com>
2024-11-15 09:34:07 +0000

    réparer betise yann

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,31 +1,21 @@
 const dotenv = require('dotenv');
-const { Sequelize } = require('sequelize');
 
 // Load environment variables from .env file
 dotenv.config();
 
-// Créer une instance Sequelize avec les informations de configuration
-const sequelize = new Sequelize(
-  process.env.PGDATABASE,
-  process.env.PGUSERNAME,
-  process.env.PGPASSWORD,
-  {
+module.exports = {
+  development: {
+    username: process.env.PGUSERNAME,
+    password: process.env.PGPASSWORD,
+    database: process.env.PGDATABASE,
     host: process.env.PGHOST,
     dialect: 'postgres',
     port: process.env.DB_PORT || 5432,
     dialectOptions: {
       ssl: {
         require: true,
-        rejectUnauthorized: false, // Utile pour le développement local
+        rejectUnauthorized: false, // This line is crucial for local development
       },
     },
-  }
-);
-
-// Tester la connexion
-sequelize
-  .authenticate()
-  .then(() => console.log('Connexion réussie à PostgreSQL'))
-  .catch(err => console.error('Impossible de se connecter à PostgreSQL', err));
-
-module.exports = sequelize;
+  },
+};
--- backend/index.js
+++ backend/index.js
@@ -1,43 +1,8 @@
-const { DataTypes } = require('sequelize');
-const sequelize = require('../config/db');  // Référence directe à l'instance Sequelize exportée depuis db.js
+// backend/server.js
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
-<<<<<<< HEAD
-const User = sequelize.define('user', {
-  user_id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    autoIncrement: true,
-    primaryKey: true,
-  },
-  username: {
-    type: DataTypes.STRING,
-    allowNull: true,
-    unique: true,
-  },
-  email: {
-    type: DataTypes.STRING,
-    allowNull: true,
-    unique: true,
-    validate: {
-      isEmail: true,
-    },
-  },
-  password: {
-    type: DataTypes.STRING,
-    allowNull: true,
-  },
-  created_at: {
-    type: DataTypes.DATE,
-    defaultValue: DataTypes.NOW,
-  },
-  last_login: {
-    type: DataTypes.DATE,
-    allowNull: true,
-  },
-}, {
-  tableName: 'users',  // Nom de la table dans la base de données
-  timestamps: false,   // Désactivation des champs createdAt et updatedAt auto-générés
-=======
 const app = express();
 
 // Configuration
@@ -49,18 +14,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Routes
 const authRoutes = require('./routes/auth');
 const userRoutes = require('./routes/users');
-const mooviesRoutes = require('./routes/moovies');
 
 app.use('/auth', authRoutes);
 app.use('/users', userRoutes);
-app.use('/moovies', mooviesRoutes);
 
 // Démarrer le serveur
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Serveur démarré sur le port ${PORT}`);
->>>>>>> 8a77598652c135e3d6b7d01432368cc41e3f3822
 });
-
-module.exports = User;
-
