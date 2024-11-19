e65b00b48e96b48189cc42d119a1f7e87b0c774b
xanax971 <christophe.yann.971@gmail.com>
2024-11-19 11:42:18 +0100

    Ajoute une route dans routes/preferences.js pour permettre à un utilisateur d'ajouter une préférence.

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const { syncModels } = require('./models'); // Importer la fonction de synchronisation
 
 const app = express();
 
@@ -10,7 +11,6 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
 // Routes
 const authRoutes = require('./routes/auth');
 const userRoutes = require('./routes/users');
@@ -18,8 +18,13 @@ const userRoutes = require('./routes/users');
 app.use('/auth', authRoutes);
 app.use('/users', userRoutes);
 
-// Démarrer le serveur
+// Démarrer le serveur et synchroniser les modèles
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
   console.log(`Serveur démarré sur le port ${PORT}`);
+  try {
+    await syncModels(); // Synchroniser les modèles
+  } catch (error) {
+    console.error('Erreur lors de la synchronisation des modèles :', error);
+  }
 });
